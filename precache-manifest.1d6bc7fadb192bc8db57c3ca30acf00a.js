f47f4133e81804663581d106952756b2e833b976
hemant garg <hemant.gr6@gmail.com>
2019-10-04 19:12:59 +0530

    Updates

---
+++ precache-manifest.1d6bc7fadb192bc8db57c3ca30acf00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec1b44ac98ef9bff1c64d9fe1753c46",
+    "url": "/whitepanda-task/index.html"
+  },
+  {
+    "revision": "d509d9334b5df7bc6fc5",
+    "url": "/whitepanda-task/static/css/main.070c91b5.chunk.css"
+  },
+  {
+    "revision": "2316da31c8e8127201c1",
+    "url": "/whitepanda-task/static/js/2.c5aa52e8.chunk.js"
+  },
+  {
+    "revision": "d509d9334b5df7bc6fc5",
+    "url": "/whitepanda-task/static/js/main.48018ca9.chunk.js"
+  },
+  {
+    "revision": "29d210f16a35c59a4273",
+    "url": "/whitepanda-task/static/js/runtime-main.f21f7ec9.js"
+  },
+  {
+    "revision": "09d513a28c6227faec91a730452fb048",
+    "url": "/whitepanda-task/static/media/analytics.09d513a2.svg"
+  }
+]);
